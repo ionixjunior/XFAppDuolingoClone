88f4ab58640d1963d5dd3ac1ad25b61f4519d0eb
Ione Souza Junior <junior@ionixjunior.com.br>
2019-08-17 11:18:56 -0300

    Refatorar criação da main page

--- AppDuolingoClone/App.xaml.cs
+++ AppDuolingoClone/App.xaml.cs
@@ -18,7 +18,8 @@ namespace AppDuolingoClone
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            var mainPage = $"{nameof(NavigationPage)}/{nameof(MainPage)}";
+            await NavigationService.NavigateAsync(mainPage);
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
