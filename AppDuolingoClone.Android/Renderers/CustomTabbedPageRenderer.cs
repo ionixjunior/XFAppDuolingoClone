275afc80cfcf2ac95d13bf055323af2556aa1dab
Ione Souza Junior <junior@ionixjunior.com.br>
2019-08-24 12:30:27 -0300

    Customizar ícones das abas do Android

--- AppDuolingoClone.Android/Renderers/CustomTabbedPageRenderer.cs
+++ AppDuolingoClone.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using Android.Content;
+using Android.Support.Design.BottomNavigation;
 using Android.Support.Design.Widget;
 using AppDuolingoClone.Droid.Renderers;
+using AppDuolingoClone.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms.Platform.Android.AppCompat;
@@ -11,6 +13,9 @@ namespace AppDuolingoClone.Droid.Renderers
 {
     public class CustomTabbedPageRenderer : TabbedPageRenderer
     {
+        private TabbedPage _formsTabs;
+        private BottomNavigationView _bottomNavigationView;
+
         public CustomTabbedPageRenderer(Context context) : base(context)
         {
         }
@@ -21,10 +26,85 @@ namespace AppDuolingoClone.Droid.Renderers
 
             if (e.NewElement != null)
             {
+                _formsTabs = Element;
+                _formsTabs.CurrentPageChanged += OnCurrentPageChanged;
+
                 var relativeLayout = base.GetChildAt(0) as Android.Widget.RelativeLayout;
-                var bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
-                bottomNavigationView.SetMinimumHeight(300);
+                _bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
+                _bottomNavigationView.SetMinimumHeight(300);
+                _bottomNavigationView.ItemIconTintList = null;
+                _bottomNavigationView.ItemIconSize = 150;
+                _bottomNavigationView.SetShiftMode(false, false);
+                _bottomNavigationView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
+
+                UpdateAllTabs();
             }
+
+            if (e.OldElement != null)
+            {
+                _formsTabs.CurrentPageChanged -= OnCurrentPageChanged;
+            }
+        }
+
+        private void UpdateAllTabs()
+        {
+            for (var index = 0; index < _formsTabs.Children.Count; index++)
+            {
+                var androidTab = _bottomNavigationView.Menu.GetItem(index);
+
+                if (_formsTabs.Children[index] is LessonsView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_lessons_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_lessons);
+                    continue;
+                }
+
+                if (_formsTabs.Children[index] is ProfileView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_profile_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_profile);
+                    continue;
+                }
+
+                if (_formsTabs.Children[index] is RankingView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_ranking_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_ranking);
+                    continue;
+                }
+
+                if (_formsTabs.Children[index] is StoreView)
+                {
+                    if (_formsTabs.Children[index] == _formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_store_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_store);
+                    continue;
+                }
+            }
+        }
+
+        private void OnCurrentPageChanged(object sender, EventArgs e)
+        {
+            UpdateAllTabs();
         }
     }
 }
