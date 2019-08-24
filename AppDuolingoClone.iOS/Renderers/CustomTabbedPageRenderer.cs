6ca5c33bb0e570e012416b85e9a11451f3ce5c6a
Ione Souza Junior <junior@ionixjunior.com.br>
2019-08-24 10:44:48 -0300

    Aumentar tamanho da TabbedPage no iOS

---
+++ AppDuolingoClone.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AppDuolingoClone.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace AppDuolingoClone.iOS.Renderers
+{
+    public class CustomTabbedPageRenderer : TabbedRenderer
+    {
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            var tabFrame = TabBar.Frame;
+
+            var tabHeight = 150;
+            tabFrame.Height = tabHeight;
+            tabFrame.Y = View.Frame.Height - tabHeight;
+
+            TabBar.Frame = tabFrame;
+        }
+    }
+}
