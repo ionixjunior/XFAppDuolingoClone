955e21828cb9486dbe7571a077c1b4c262691155
Ione Souza Junior <junior@ionixjunior.com.br>
2019-09-15 12:44:50 -0300

    Configurar barra de status do iOS

---
+++ AppDuolingoClone.iOS/Renderers/CustomContentPageRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AppDuolingoClone.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CustomContentPageRenderer))]
+namespace AppDuolingoClone.iOS.Renderers
+{
+    public class CustomContentPageRenderer : PageRenderer
+    {
+        public override bool PrefersStatusBarHidden()
+        {
+            return true;
+        }
+    }
+}
