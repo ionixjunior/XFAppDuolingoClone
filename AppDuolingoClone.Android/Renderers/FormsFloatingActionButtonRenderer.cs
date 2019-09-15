ec0f812eede40f13156deb889eae948afb9796e5
Ione Souza Junior <junior@ionixjunior.com.br>
2019-09-15 11:55:39 -0300

    Configurar ícone do floating action button

--- AppDuolingoClone.Android/Renderers/FormsFloatingActionButtonRenderer.cs
+++ AppDuolingoClone.Android/Renderers/FormsFloatingActionButtonRenderer.cs
@@ -27,6 +27,7 @@ namespace AppDuolingoClone.Droid.Renderers
                 _floatingActionButton = new FloatingActionButton(Context);
                 _floatingActionButton.UseCompatPadding = true;
                 ConfigureBackgroundColor();
+                ConfigureImage();
                 _floatingActionButton.Click += OnFabClick;
 
                 SetNativeControl(_floatingActionButton);
@@ -41,6 +42,19 @@ namespace AppDuolingoClone.Droid.Renderers
             var floatingActionButtonColor = Element.BackgroundColor.ToAndroid();
             _floatingActionButton.BackgroundTintList = ColorStateList.ValueOf(floatingActionButtonColor);
             Element.BackgroundColor = Color.Transparent;
+        }
+
+        private void ConfigureImage()
+        {
+            if (Element == null)
+                return;
+
+            var fileName = (Element.ImageSource as FileImageSource)?.File;
+            if (fileName == null)
+                return;
+
+            var resourceId = Resources.GetIdentifier(fileName, "drawable", Context.PackageName);
+            _floatingActionButton.SetImageResource(resourceId);
         }
 
         private void OnFabClick(object sender, EventArgs e)
