35ec96add38f284099a7a8278d57ab27c38f55f5
Ione Souza Junior <junior@ionixjunior.com.br>
2019-10-12 11:57:02 -0300

    Adicionar conversor para controlar ícone da coroa

---
+++ AppDuolingoClone/Converters/LevelToImageCrownConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AppDuolingoClone.Converters
+{
+    public class LevelToImageCrownConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var level = value as string;
+
+            if (level == string.Empty)
+                return "crown_gray_stroke";
+
+            return "crown_stroke";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
