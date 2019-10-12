0c0923c273e0787ace8397b56a3cd21a8013d4a1
Ione Souza Junior <junior@ionixjunior.com.br>
2019-10-12 12:05:16 -0300

    Adicionar conversor para controlar cor da lição

---
+++ AppDuolingoClone/Converters/LevelToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AppDuolingoClone.Converters
+{
+    public class LevelToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var level = value as string;
+
+            if (level == "4")
+                return "#f19a37";
+
+            return "#c287f8";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
