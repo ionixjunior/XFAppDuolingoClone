a5be6e602cad929227fae4bf6c4e4b11ec895a4d
Ione Souza Junior <junior@ionixjunior.com.br>
2019-10-19 15:50:55 -0300

    Mapeado novas cores das lições

--- AppDuolingoClone/Fakes/LessonServiceFake.cs
+++ AppDuolingoClone/Fakes/LessonServiceFake.cs
@@ -9,6 +9,14 @@ namespace AppDuolingoClone.Fakes
 {
     public class LessonServiceFake : ILessonService
     {
+        private string _colorLevel5 = "#f7c745";
+        private string _colorLevel4 = "#f19a37";
+        private string _colorLevel3 = "#ec5954";
+        private string _colorLevel2 = "#8bc63b";
+        private string _colorLevel1 = "#4faef0";
+        private string _colorLevel0 = "#c287f8";
+        private string _colorBonus = "#ffffff";
+
         public async Task<IList<LessonGroup>> GetLessonsGroup()
         {
             return await Task.Run(() =>
@@ -20,7 +28,17 @@ namespace AppDuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Single,
                         Lessons = new List<Lesson>
                         {
-                            GetNewLesson("Introdução", "4", "lesson_egg", "#f19a37")
+                            GetNewLesson("Introdução", "5", "lesson_egg", _colorLevel5)
+                        }
+                    },
+
+                    new LessonGroup
+                    {
+                        Type = LessonGroupTypeEnum.Multi,
+                        Lessons = new List<Lesson>
+                        {
+                            GetNewLesson("Saudações", "4", "lesson_dialog", _colorLevel4),
+                            GetNewLesson("Viagem", "3", "lesson_airplane", _colorLevel3)
 
 
                         }
@@ -31,20 +49,8 @@ namespace AppDuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Multi,
                         Lessons = new List<Lesson>
                         {
-                            GetNewLesson("Saudações", "4", "lesson_dialog", "#f19a37"),
-                            GetNewLesson("Viagem", string.Empty, "lesson_airplane", "#c287f8")
-
-
-                        }
-                    },
-
-                    new LessonGroup
-                    {
-                        Type = LessonGroupTypeEnum.Multi,
-                        Lessons = new List<Lesson>
-                        {
-                            GetNewLesson("Cafeteria", string.Empty, "lesson_hamburger", "#c287f8"),
-                            GetNewLesson("Famílias", string.Empty, "lesson_baby", "#c287f8")
+                            GetNewLesson("Cafeteria", "2", "lesson_hamburger", _colorLevel2),
+                            GetNewLesson("Famílias", "1", "lesson_baby", _colorLevel1)
 
                         }
                     },
@@ -54,9 +60,9 @@ namespace AppDuolingoClone.Fakes
                         Type = LessonGroupTypeEnum.Bonus,
                         Lessons = new List<Lesson>
                         {
-                            GetNewLesson("Bônus", string.Empty, "lesson_plus", "#ffffff"),
-                            GetNewLesson("Bônus", string.Empty, "lesson_plus", "#ffffff"),
-                            GetNewLesson("Bônus", string.Empty, "lesson_plus", "#ffffff")
+                            GetNewLesson("Bônus", string.Empty, "lesson_plus", _colorBonus),
+                            GetNewLesson("Bônus", string.Empty, "lesson_plus", _colorBonus),
+                            GetNewLesson("Bônus", string.Empty, "lesson_plus", _colorBonus)
                         }
                     }
                 };
