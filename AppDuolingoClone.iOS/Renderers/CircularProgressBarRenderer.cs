f1a0bcfe053d5f8d9bbd6dd6a9103a10d9fdf2c9
Ione Souza Junior <junior@ionixjunior.com.br>
2019-11-02 10:30:14 -0300

    Criar renderer do iOS para o circular progress bar

---
+++ AppDuolingoClone.iOS/Renderers/CircularProgressBarRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AppDuolingoClone.Controls;
+using AppDuolingoClone.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CircularProgressBar), typeof(CircularProgressBarRenderer))]
+namespace AppDuolingoClone.iOS.Renderers
+{
+    public class CircularProgressBarRenderer : ViewRenderer<CircularProgressBar, UIView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<CircularProgressBar> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
