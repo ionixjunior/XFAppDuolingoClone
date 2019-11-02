9d1ec132285a6aec12ef0da84a1cb32dc83e2138
Ione Souza Junior <junior@ionixjunior.com.br>
2019-11-02 11:52:38 -0300

    Aumentar largura da linha da progress bar

--- AppDuolingoClone.iOS/Controls/CircularProgressBariOS.cs
+++ AppDuolingoClone.iOS/Controls/CircularProgressBariOS.cs
@@ -42,14 +42,14 @@ namespace AppDuolingoClone.iOS.Controls
             _trackLayer.Path = circlePath.CGPath;
             _trackLayer.FillColor = UIColor.Clear.CGColor;
             _trackLayer.StrokeColor = trackColor;
-            _trackLayer.LineWidth = (nfloat)5.0;
+            _trackLayer.LineWidth = (nfloat)8.0;
             _trackLayer.StrokeEnd = (nfloat)1.0;
             Layer.AddSublayer(_trackLayer);
 
             _progressLayer.Path = circlePath.CGPath;
             _progressLayer.FillColor = UIColor.Clear.CGColor;
             _progressLayer.StrokeColor = progressColor;
-            _progressLayer.LineWidth = (nfloat)5.0;
+            _progressLayer.LineWidth = (nfloat)8.0;
             _progressLayer.StrokeEnd = (nfloat)progress;
             Layer.AddSublayer(_progressLayer);
         }
