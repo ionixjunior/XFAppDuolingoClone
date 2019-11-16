794deb7413aa765e60f3b8f7072dc8869e691c69
Ione Souza Junior <junior@ionixjunior.com.br>
2019-11-16 11:58:35 -0300

    Implementar IActivateAware na viewmodel das histórias

--- AppDuolingoClone/ViewModels/StoriesViewModel.cs
+++ AppDuolingoClone/ViewModels/StoriesViewModel.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using AppDuolingoClone.Interfaces;
+using Prism;
 
 namespace AppDuolingoClone.ViewModels
 {
-    public class StoriesViewModel : ViewModelBase
+    public class StoriesViewModel : ViewModelBase, IActiveAware
     {
         private readonly IStoriesService _storiesService;
+
+        private bool _isActive;
+        public bool IsActive
+        {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value, RaiseIsActivatedChanged);
+        }
+
+        public event EventHandler IsActiveChanged;
 
         public StoriesViewModel(IStoriesService storiesService)
         {
             _storiesService = storiesService;
+        }
+
+        private void RaiseIsActivatedChanged()
+        {
+            if (IsActive)
+            {
+                System.Diagnostics.Debug.WriteLine("Evento da aba");
+            }
         }
     }
 }
