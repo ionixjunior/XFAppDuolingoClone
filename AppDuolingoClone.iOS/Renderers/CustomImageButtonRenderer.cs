d79e3d2e3616915a3d1cdd722655aed8aea245be
Ione Souza Junior <junior@ionixjunior.com.br>
2019-11-23 10:56:46 -0300

    Remover estado de seleção dos ImageButtons do iOS

---
+++ AppDuolingoClone.iOS/Renderers/CustomImageButtonRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AppDuolingoClone.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ImageButton), typeof(CustomImageButtonRenderer))]
+namespace AppDuolingoClone.iOS.Renderers
+{
+    public class CustomImageButtonRenderer : ImageButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ImageButton> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.AdjustsImageWhenHighlighted = false;
+            }
+        }
+
+        protected override UIButton CreateNativeControl()
+        {
+            return new UIButton(UIButtonType.Custom);
+        }
+    }
+}
