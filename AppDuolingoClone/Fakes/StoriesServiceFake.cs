f4da33dc93b73d8c0bda7d129570311cdc99b9d5
Ione Souza Junior <junior@ionixjunior.com.br>
2019-11-23 11:23:12 -0300

    Mapear cores da sombra da história

--- AppDuolingoClone/Fakes/StoriesServiceFake.cs
+++ AppDuolingoClone/Fakes/StoriesServiceFake.cs
@@ -18,22 +18,23 @@ namespace AppDuolingoClone.Fakes
                         "Série 1",
                         new List<Stories>()
                         {
-                            GetNewStories("Bom dia!", "stories_coffe"),
-                            GetNewStories("Um encontro", "stories_candle"),
-                            GetNewStories("Uma coisa", "stories_bread"),
-                            GetNewStories("Surpresa", "stories_gift")
+                            GetNewStories("Bom dia!", "stories_coffe", "#f5b43f"),
+                            GetNewStories("Um encontro", "stories_candle", "#503322"),
+                            GetNewStories("Uma coisa", "stories_bread", "#68ad33"),
+                            GetNewStories("Surpresa", "stories_gift", "#de90d0")
                         }
                     )
                 };
             });
         }
 
-        private Stories GetNewStories(string name, string image)
+        private Stories GetNewStories(string name, string image, string shadowBottomColor)
         {
             return new Stories
             {
                 Name = name,
-                Image = image
+                Image = image,
+                ShadowBottomColor = shadowBottomColor
             };
         }
     }
--- AppDuolingoClone/Models/Stories.cs
+++ AppDuolingoClone/Models/Stories.cs
@@ -6,5 +6,6 @@ namespace AppDuolingoClone.Models
     {
         public string Name { get; set; }
         public string Image { get; set; }
+        public string ShadowBottomColor { get; set; }
     }
 }
