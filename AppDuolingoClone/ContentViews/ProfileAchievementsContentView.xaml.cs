5fb562306dcfbf0b1ffaffec1734ab533ea94f4b
Ione Souza Junior <junior@ionixjunior.com.br>
2019-11-30 12:04:18 -0300

    Adicionar views das conquistas e amigos da tela do perfil

---
+++ AppDuolingoClone/ContentViews/ProfileAchievementsContentView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppDuolingoClone.ContentViews
+{
+    public partial class ProfileAchievementsContentView : ContentView
+    {
+        public ProfileAchievementsContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
