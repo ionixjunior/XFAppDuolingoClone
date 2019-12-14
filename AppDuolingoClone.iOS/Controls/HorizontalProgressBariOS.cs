3e93a9dfbe5ff3d8d7aef162b80ce1c6765de977
Ione Souza Junior <junior@ionixjunior.com.br>
2019-12-14 12:10:05 -0300

    Criar render da progress bar horizontal no iOS

---
+++ AppDuolingoClone.iOS/Controls/HorizontalProgressBariOS.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using UIKit;
+
+namespace AppDuolingoClone.iOS.Controls
+{
+    public class HorizontalProgressBariOS : UIView
+    {
+        private CAShapeLayer _progressLayer = new CAShapeLayer();
+        private CAShapeLayer _trackLayer = new CAShapeLayer();
+        private double _width;
+        private double _height;
+        private CGColor _trackColor;
+        private CGColor _progressColor;
+        private double _progress;
+
+        public HorizontalProgressBariOS(
+            double width,
+            double height,
+            CGColor trackColor,
+            CGColor progressColor,
+            double progress)
+        {
+            _width = width;
+            _height = height;
+            _trackColor = trackColor;
+            _progressColor = progressColor;
+            _progress = progress;
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            MakeHorizontalPath();
+        }
+
+        private void MakeHorizontalPath()
+        {
+            BackgroundColor = UIColor.Clear;
+            Layer.CornerRadius = (nfloat)(_height / 2);
+            Layer.MasksToBounds = true;
+
+            var path = new UIBezierPath();
+
+            path.MoveTo(new CGPoint(0, _height / 2));
+            path.AddLineTo(new CGPoint(Frame.Width, _height / 2));
+
+            _trackLayer.Path = path.CGPath;
+            _trackLayer.FillColor = UIColor.Clear.CGColor;
+            _trackLayer.StrokeColor = _trackColor;
+            _trackLayer.LineWidth = (nfloat)_height;
+            _trackLayer.StrokeEnd = (nfloat)1.0;
+            Layer.AddSublayer(_trackLayer);
+
+            _progressLayer.Path = path.CGPath;
+            _progressLayer.FillColor = UIColor.Clear.CGColor;
+            _progressLayer.StrokeColor = _progressColor;
+            _progressLayer.LineWidth = (nfloat)_height;
+            _progressLayer.StrokeEnd = (nfloat)_progress;
+            _progressLayer.LineCap = CAShapeLayer.CapRound;
+            Layer.AddSublayer(_progressLayer);
+
+            path.ClosePath();
+        }
+    }
+}
---
+++ AppDuolingoClone.iOS/Renderers/HorizontalProgressBarRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AppDuolingoClone.Controls;
+using AppDuolingoClone.iOS.Controls;
+using AppDuolingoClone.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HorizontalProgressBar), typeof(HorizontalProgressBarRenderer))]
+namespace AppDuolingoClone.iOS.Renderers
+{
+    public class HorizontalProgressBarRenderer : ViewRenderer<HorizontalProgressBar, HorizontalProgressBariOS>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<HorizontalProgressBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control is null)
+            {
+                if (Element is null)
+                    return;
+
+                var nativeControl = new HorizontalProgressBariOS(
+                    Element.WidthRequest,
+                    Element.HeightRequest,
+                    Element.TrackColor.ToCGColor(),
+                    Element.ProgressColor.ToCGColor(),
+                    Element.Progress
+                );
+
+                SetNativeControl(nativeControl);
+            }
+        }
+    }
+}
