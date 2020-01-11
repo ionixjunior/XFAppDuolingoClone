9cba6b3e3da1335dbcdae82ff4500294b5d634a7
Ione Souza Junior <junior@ionixjunior.com.br>
2020-01-11 11:32:25 -0300

    Criar conversor para ajustar cor do level da conquista

---
+++ AppDuolingoClone/Converters/AchievementsActiveToTextColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AppDuolingoClone.Converters
+{
+    public class AchievementsActiveToTextColorConverter : IValueConverter
+    {
+        private Color _activeColor = Color.White;
+        private Color _finishedColor = Color.FromHex("#cc7700");
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isActive)
+            {
+                if (isActive)
+                    return _activeColor;
+
+                return _finishedColor;
+            }
+
+            return _activeColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
