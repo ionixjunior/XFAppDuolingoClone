0d75c85adbc1ebb790e40c4d4c32aa7e0149d29d
Ione Souza Junior <junior@ionixjunior.com.br>
2020-02-08 11:24:41 -0300

    Implementar carregamento dos itens da loja na viewmodel

--- AppDuolingoClone/ViewModels/StoreViewModel.cs
+++ AppDuolingoClone/ViewModels/StoreViewModel.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using AppDuolingoClone.Interfaces;
+using AppDuolingoClone.Models;
+using Prism;
+
 namespace AppDuolingoClone.ViewModels
 {
-    public class StoreViewModel : ViewModelBase
+    public class StoreViewModel : ViewModelBase, IActiveAware
     {
-        public StoreViewModel()
+        private readonly IStoreService _storeService;
+
+        private bool _isActive;
+        public bool IsActive
         {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value, RaiseIsActivatedChanged);
+        }
+
+        public event EventHandler IsActiveChanged;
+
+        public List<StoreItemGroup> Groups { get; private set; }
+
+        public StoreViewModel(IStoreService storeService)
+        {
+            _storeService = storeService;
+            Groups = new List<StoreItemGroup>();
+        }
+
+        private async void RaiseIsActivatedChanged()
+        {
+            if (IsActive)
+            {
+                if (!Groups.Any())
+                {
+                    var storeGroups = await _storeService.GetItems();
+                    Groups.AddRange(storeGroups);
+                }
+            }
         }
     }
 }
